04305aeee4499a4ee1eed1d6a1cf53e1008e17e2
pavankotas <pavankotas@gmail.com>
2019-04-09 22:42:40 -0500

    Merged local version of Dharani and completing checkin

--- DP_PugAssignment/app.js
+++ DP_PugAssignment/app.js
@@ -1,5 +1,21 @@
 var express = require('express');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+    cors = require('cors'),
+    path = require('path');
+
+
 var app = express();
+
+var bodyParser = require('body-parser');
+
+// configure the app to use bodyParser()
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
 
 //Listening to nodeJS Application
 const PORT= 3000;
@@ -14,8 +30,21 @@ app.set('view engine', 'pug');
 
 //Setting public folder
 app.use(express.static('public'));
+
 /*app.use('/bootstrap', express.static(__dirname, + '/node_modules/bootstrap/dist/css/'));*/
 //Default Route
 app.get('/', function(req, res){
     res.render('login')
+});
+
+
+app.post('/details', function(req, res){
+    console.log('Cookies: ', req.cookies);
+    res.render('Details', {user: req.cookies.user});
+});
+
+app.post('/', function(req, res){
+    console.log("Body");
+    console.log(req.body);
+    res.cookie('user', req.body).render('login');
 });
